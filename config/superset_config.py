7abf3eb8d25cb3279fb9c4f006cf4c7927daa966
yasinerendursun <dursun.y.eren@gmail.com>
2024-12-04 19:51:31 +0300

    updates CONTENT_SECURITY_POLICY

--- config/superset_config.py
+++ config/superset_config.py
@@ -8,10 +8,5 @@ ENABLE_PROXY_FIX = True
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# Add Content Security Policy to allow images from all sources
-CONTENT_SECURITY_POLICY = {
-    "default-src": ["'self'"],
-    "img-src": ["*"],  # Allow images from any domain
-    "script-src": ["'self'", "'unsafe-inline'"],
-    "style-src": ["'self'", "'unsafe-inline'"],
-}
+# Temporarily disable restrictive CSP for debugging
+CONTENT_SECURITY_POLICY = None
