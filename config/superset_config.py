ea6fb8e721f8bf21572b58c5198f5e3f0e5ac0b0
yasinerendursun <dursun.y.eren@gmail.com>
2024-12-04 19:14:36 +0300

    adds CONTENT_SECURITY_POLICY

--- config/superset_config.py
+++ config/superset_config.py
@@ -7,3 +7,11 @@ FEATURE_FLAGS = {
 ENABLE_PROXY_FIX = True
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
+
+# Add Content Security Policy to allow images from all sources
+CONTENT_SECURITY_POLICY = {
+    "default-src": ["'self'"],
+    "img-src": ["*"],  # Allow images from any domain
+    "script-src": ["'self'", "'unsafe-inline'"],
+    "style-src": ["'self'", "'unsafe-inline'"],
+}
