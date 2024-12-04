e3b697ae095b6ffca9eee5787ce2697051906fd4
yasinerendursun <dursun.y.eren@gmail.com>
2024-12-04 19:56:06 +0300

    updates CONTENT_SECURITY_POLICY

--- config/superset_config.py
+++ config/superset_config.py
@@ -8,5 +8,18 @@ ENABLE_PROXY_FIX = True
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# Temporarily disable restrictive CSP for debugging
-CONTENT_SECURITY_POLICY = None
+# Override default Superset CSP settings
+WTF_CSRF_ENABLED = True
+CONTENT_SECURITY_POLICY = {}  # Empty dict to disable default CSP
+TALISMAN_ENABLED = False  # Disable Talisman (Flask-Security wrapper)
+CONTENT_SECURITY_POLICY_ENABLED = False  # Explicitly disable CSP
+
+# If the above doesn't work, we provide a permissive CSP as fallback
+if CONTENT_SECURITY_POLICY_ENABLED:
+    CONTENT_SECURITY_POLICY = {
+        "default-src": ["'*'", "'unsafe-inline'", "'unsafe-eval'"],
+        "img-src": ["'*'", "data:", "blob:", "'self'"],
+        "connect-src": ["'*'"],
+        "script-src": ["'*'", "'unsafe-inline'", "'unsafe-eval'"],
+        "style-src": ["'*'", "'unsafe-inline'"],
+    }
